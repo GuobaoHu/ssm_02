fba029308bb15cf9489102f2bddede8ef7a893b7
guyue <guyue@guyue-PC>
2018-04-02 21:45:43 +0800

    关于controller方法的返回值问题测试

--- ssm_02/src/main/java/com/guyue/ssm/controller/ItemsCustomController.java
+++ ssm_02/src/main/java/com/guyue/ssm/controller/ItemsCustomController.java
@@ -2,8 +2,12 @@ package com.guyue.ssm.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,6 +33,7 @@ public class ItemsCustomController {
 	}
 	
 	//商品修改界面的商品详情展现
+	//情况一：返回ModelAndView
 	@RequestMapping("editItems")
 	public ModelAndView editItems() throws Exception {
 		
@@ -44,6 +49,36 @@ public class ItemsCustomController {
 		modelAndView.setViewName("editItems.jsp");
 		
 		return modelAndView;
+	}
+	
+	//情况二：返回字符串
+	//2.1返回逻辑视图
+	@RequestMapping("editItemsReturnString")
+	public String editItemsReturnString(Model model) throws Exception {
+		
+			
+		//查询商品信息
+		ItemsCustom itemsCustom = itemsCustomService.findItemsById(1);
+		
+		//将商品信息(model)放到modelAndView中
+		model.addAttribute("itemsCustom", itemsCustom);
+		
+		return "editItems.jsp";
+	}
+	
+	//2.2返回redirect或forward的字符串
+	@RequestMapping("redirectToSubmitSuccess")
+	public String redirectToSubmitSuccess() throws Exception {
+		/*return "redirect:queryItems.action";*/
+		return "forward:queryItems.action";
+	}
+	
+	
+	//情况三：没有返回值void
+	@RequestMapping("nonReturn")
+	public void nonReturn(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//request.getRequestDispatcher("queryItems.action").forward(request, response);
+		response.sendRedirect("queryItems.action");
 	}
 	
 	//商品修改成功的提示
